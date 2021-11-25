7293380882ef38bbb135193e4ff3512c4f9ee311
Eise Zimmerman <eise.zimmerman@outlook.com>
2021-11-25 18:43:06 +0100

    Put funcion body definition in namespace

--- src/indexBuffer.cpp
+++ src/indexBuffer.cpp
@@ -1,8 +1,11 @@
 #include "indexBuffer.hpp"
 
-IndexBuffer::IndexBuffer(long long int size, const void *data, unsigned int usage)
+namespace Engine
 {
-  glGenBuffers(1, &m_RendererID);
-  bind();
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, usage);
+  IndexBuffer::IndexBuffer(long long int size, const void *data, unsigned int usage)
+  {
+    glGenBuffers(1, &m_RendererID);
+    bind();
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, usage);
+  }
 }
--- src/vertexBuffer.cpp
+++ src/vertexBuffer.cpp
@@ -1,8 +1,11 @@
 #include "vertexBuffer.hpp"
 
-VertexBuffer::VertexBuffer(long long int size, const void *data, unsigned int usage)
+namespace Engine
 {
-  glGenBuffers(1, &m_RendererID);
-  bind();
-  glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+  VertexBuffer::VertexBuffer(long long int size, const void *data, unsigned int usage)
+  {
+    glGenBuffers(1, &m_RendererID);
+    bind();
+    glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+  }
 }
